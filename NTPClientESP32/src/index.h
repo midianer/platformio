0dcc89e7f9a57ab48ed992ae77f6a330f4ac204e
Guenter Kern B550 <guenter-kern@t-online.de>
2026-05-07 11:38:38 +0200

    NTP working

---
+++ NTPClientESP32/src/index.h
@@ -0,0 +1,18 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<head>
+  <title>S2Mini NTP</title>
+  <style>
+  </style>
+</head>
+<html>
+<body>
+
+<div id="demo">
+<h1>S2Mini NTP</h1>
+<h2>Guenter Kern Ingbuero</h2>
+</div>
+
+</body>
+</html>
+)=====";
